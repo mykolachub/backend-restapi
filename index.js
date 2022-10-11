ca5d77394f79cdc0702de96efe7dad684994a651
Nikolai <nikolaichub.me@gmail.com>
2022-10-11 17:00:30 +0300

    feat: basic structure for express server

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 'use strict';
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+
+const server = express();
+
+// Middlewares
+server.use(express.json());
+server.use(morgan('dev'));
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://127.0.0.1:${PORT}`);
+});
+
+// Unhandled Application Errors
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
