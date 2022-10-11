74fdd557dbe36722412b2148f5f46de1110bd4a4
Nikolai <nikolaichub.me@gmail.com>
2022-10-11 18:47:32 +0300

    feat: records end point

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 
 const userRouter = require('./src/routes/userRouter');
 const categoryRouter = require('./src/routes/categoryRouter');
+const recordRouter = require('./src/routes/recordRouter');
 
 const server = express();
 
@@ -15,6 +16,7 @@ server.use(morgan('dev'));
 
 server.use('/api/v1/users', userRouter);
 server.use('/api/v1/categories', categoryRouter);
+server.use('/api/v1/records', recordRouter);
 
 server.all('*', (req, res) => {
   res.status(404).json({
