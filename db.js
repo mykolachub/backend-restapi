4fb0b21ef7d0be166d85c7fd9993430ff3279211
Nikolai <nikolaichub.me@gmail.com>
2022-11-06 00:33:39 +0200

    feat: universal models registration and models associations

--- db.js
+++ db.js
@@ -2,9 +2,40 @@
 
 const { Sequelize } = require('sequelize');
 
+const { readdir } = require('node:fs/promises');
+const path = require('node:path');
+
+const modelsPath = path.join(process.cwd(), './src/models');
+
+// Connection
 const sequelize = new Sequelize('backend_restapi', 'postgres', 'root', {
   host: 'localhost',
   dialect: 'postgres',
+  logging: false,
 });
+
+// Models registration
+(async () => {
+  try {
+    const files = await readdir(modelsPath);
+    for (const file of files) {
+      const modulePath = path.join(modelsPath, file);
+      require(modulePath)(sequelize);
+    }
+
+    const { user, category, record } = sequelize.models;
+
+    // Associations
+    user.hasOne(record);
+    record.belongsTo(user);
+
+    category.hasOne(record);
+    record.belongsTo(category);
+
+    await sequelize.sync({ force: true });
+  } catch (err) {
+    console.error(err);
+  }
+})();
 
 module.exports = sequelize;
