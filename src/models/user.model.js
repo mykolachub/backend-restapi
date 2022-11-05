4a5a8f2956eeb0b41b900f4560e4db716c0ff730
Nikolai <nikolaichub.me@gmail.com>
2022-11-05 20:44:44 +0200

    feat: created user model

---
+++ src/models/user.model.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) =>
+  sequelize.define(
+    'user',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    { timestamps: false }
+  );
