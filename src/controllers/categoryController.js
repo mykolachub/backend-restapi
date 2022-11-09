81a1ad944817b144f3cae55eaa30bcb9e124c201
Nikolai <nikolaichub.me@gmail.com>
2022-11-09 18:20:32 +0200

    feat: categories now with userId

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,24 +1,36 @@
 'use strict';
 
+const { Op } = require('sequelize');
 const { models } = require('../../db');
 const AppError = require('../helpers/error.helper');
 
 const getAllCategories = async (req, res) => {
-  const categories = await models.category.findAll();
-  res.status(200).json({
-    status: 'success',
-    message: 'Categories successfully returned',
-    results: categories.length,
-    data: {
-      categories,
-    },
-  });
+  try {
+    const { userId } = req.query;
+    const filter = {};
+
+    if (userId) filter.userId = userId;
+    else filter.userId = { [Op.is]: null };
+
+    const categories = await models.category.findAll({ where: filter });
+    res.status(200).json({
+      status: 'success',
+      message: 'Categories successfully returned',
+      results: categories.length,
+      data: {
+        categories,
+      },
+    });
+  } catch (error) {
+    const { message, code } = AppError(error);
+    res.status(code).json(message);
+  }
 };
 
 const createCategory = async (req, res) => {
   try {
-    const { name } = req.body;
-    const category = await models.category.create({ name });
+    const { name, userId } = req.body;
+    const category = await models.category.create({ name, userId });
     res.status(200).json({
       status: 'success',
       message: 'Category successfully created',
