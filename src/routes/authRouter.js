f5f2b5f614f17e7e3f12a375cca07f1260ae3763
Nikolai <nikolaichub.me@gmail.com>
2022-12-16 14:46:11 +0200

    feat: auth router

---
+++ src/routes/authRouter.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+const authController = require('../controllers/authController');
+
+/**
+ *
+ * /api/v1/auth
+ *
+ */
+
+const authRouter = express.Router();
+
+authRouter.route('/signup').post(authController.signup);
+// authRouter.route('/login').post(authController.login);
+
+module.exports = authRouter;
