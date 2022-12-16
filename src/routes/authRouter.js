2aa2aaa0e5788250763f4181b3527b6800ead7de
Nikolai <nikolaichub.me@gmail.com>
2022-12-16 15:03:36 +0200

    feat: /api/v1/login handler

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -13,6 +13,6 @@ const authController = require('../controllers/authController');
 const authRouter = express.Router();
 
 authRouter.route('/signup').post(authController.signup);
-// authRouter.route('/login').post(authController.login);
+authRouter.route('/login').post(authController.login);
 
 module.exports = authRouter;
